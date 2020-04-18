527520a9740a63085c31ce0ccadddfd016f6f282
Anirudh Kanukanti <anirudhkanukanti@gmail.com>
2020-04-18 22:24:22 +0530

    app name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myOSApp'`, () => {
+  it(`should have as title 'angularRecursiveMenu'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('myOSApp');
+    expect(app.title).toEqual('angularRecursiveMenu');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('myOSApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularRecursiveMenu app is running!');
   });
 });
