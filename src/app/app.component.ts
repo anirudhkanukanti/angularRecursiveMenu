42d8955472b46c1bb0caf56a0bd0a80a284e1cbf
Anirudh K <anirudhkanukanti@gmail.com>
2020-04-18 21:35:46 +0530

    Recursive menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myOSApp';
+  menuData = [
+    {
+      title: 'Home', url: '', child: [{
+        title: 'More', url: '', child: [
+          { title: 'careers', url: '', child: [] },
+          { title: 'About Us', url: '', child: [{ title: 'AB', url: '', child: [] }] },
+          {
+            title: 'Locate Us', url: '', child: [{ title: 'careers', url: '', child: [] },
+            { title: 'About Us', url: '', child: [{ title: 'AB', url: '', child: [] }] },
+            { title: 'Locate Us', url: '', child: [] }]
+          }
+        ]
+      }]
+    },
+    { title: 'Contact Us', url: '', child: [] },
+    {
+      title: 'More', url: '', child: [
+        { title: 'careers', url: '', child: [] },
+        { title: 'About Us', url: '', child: [{ title: 'AB', url: '', child: [] }] },
+        {
+          title: 'Locate Us', url: '', child: [{ title: 'careers', url: '', child: [] },
+          { title: 'About Us', url: '', child: [{ title: 'AB', url: '', child: [] }] },
+          { title: 'Locate Us', url: '', child: [] }]
+        }
+      ]
+    }
+  ];
 }
---
+++ src/app/recursive-menu/recursive-menu.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-recursive-menu',
+  templateUrl: './recursive-menu.component.html',
+  styleUrls: ['./recursive-menu.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class RecursiveMenuComponent implements OnInit {
+
+  @Input() data;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
